b6ab94cee591f19329b85720fa6af0fa12286fb4
denistnguyen <denistnguyen@gmail.com>
2016-05-09 04:15:01 -0400

    update

--- Project2-Shiny/DenisNguyen/ui.R
+++ Project2-Shiny/DenisNguyen/ui.R
@@ -2,10 +2,11 @@ shinyUI(
   navbarPage(
     title = "Population Projections",
     theme = shinytheme("flatly"),
-    tabPanel("Insight: Big Picture",
+    tabPanel("Insight: Population",
       sidebarLayout(
         sidebarPanel(h3("Parameters"), style = "width:350px",
-            helpText("Customize your graph with the options below."),
+            helpText("This graph displays projected population changes within a 
+                     certain gender and race/ethnic group."),
             br(),
             selectInput("race", h4("Race:"), choices = race),
             selectInput("sex", h4("Gender:"), choices = sex),
@@ -14,7 +15,7 @@ shinyUI(
             sliderInput("year", h4("Projection Range:"), sep = "",
                 min = 2014, max = 2060, value = c(2014, 2060), ticks = FALSE)
         ),
-        mainPanel(style = "margin-left:-100px; margin-top:-100px",
+        mainPanel(style = "margin-left:-50px; margin-top:-100px",
           htmlOutput("plot")
         )
       )
@@ -22,7 +23,8 @@ shinyUI(
     tabPanel("Insight: Gender",
       sidebarLayout(
         sidebarPanel(h3("Parameters"), style = "width:350px",
-          helpText("Customize your graph with the options below."),
+          helpText("This graph displays male and female projected population in
+                   a race/ethnic group."),
           br(),
           selectInput("raceG", h4("Race:"), choices = race),
           selectInput("populationG", h4("Population:"), choices = population,
@@ -32,7 +34,7 @@ shinyUI(
           sliderInput("yearG", h4("Projection Range:"), sep = "",
                       min = 2014, max = 2060, value = c(2014, 2060), ticks = FALSE)
           ),
-          mainPanel(style = "margin-left:-100px; margin-top:-100px",
+          mainPanel(style = "margin-left:-50px; margin-top:-100px",
             htmlOutput("plotG")
           )
         )
@@ -40,7 +42,8 @@ shinyUI(
       tabPanel("Insight: Race",
         sidebarLayout(
           sidebarPanel(h3("Parameters"), style = "width:350px",
-            helpText("Customize your graph with the options below."),
+            helpText("This graph displays projected population for different
+                     races and ethnic groups."),
             br(),
             selectInput("sexR", h4("Gender:"), choices = sex),
             selectInput("populationR", h4("Population:"), choices = population),
@@ -49,16 +52,15 @@ shinyUI(
             sliderInput("yearR", h4("Projection Range:"), sep = "",
                         min = 2014, max = 2060, value = c(2014, 2060), ticks = FALSE)
           ),
-          mainPanel(style = "margin-left:-100px; margin-top:-100px",
+          mainPanel(style = "margin-left:-50px; margin-top:-100px",
             htmlOutput("plotR")
           )
         )
       ),
-#      tabPanel("Summary",
-#        verbatimTextOutput("summary")
-#      ),
-      tabPanel("Table",
+      tabPanel("Data Table",
         fluidPage(
+          p("Data was obtained from ", a("here", href="http://www.census.gov/population/projections/data/"),
+            style = "float:right"),
           column(3, selectInput("racet", h4("Select Race:"), choices = race)),
           column(3, selectInput("sext", h4("Select Gender:"), choices = sex))
       ),

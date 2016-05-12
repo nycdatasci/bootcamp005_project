4ba5eef1f0cf33c761a14995731d7c1f59a56b34
zelosa <wanda.yanli@gmail.com>
2016-05-12 03:53:13 -0400

    Version 1

--- Project2-Shiny/Glaciers_Wanda/Glaciers_Wanda/global.R
+++ Project2-Shiny/Glaciers_Wanda/Glaciers_Wanda/global.R
@@ -5,56 +5,15 @@ library(rgeos)
 library(dplyr)
 library(stringi)
 library(sp)
-library(ggvis)
 library(googleVis)
-setwd("/Users/wandawang/Desktop/glacierz")
+library(shiny)
+library(shinydashboard)
+library(leaflet)
+#setwd("./Glaciers_Wanda")
+load('./Project.RData')  #run this to work 
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00")
+               #2008,        2009,      2010,      2011,      2012,       2013,       2014
+###fix this... 
+gen.data <- select(latlong, NAME, LATITUDE, LONGITUDE) 
+gen.data <- na.omit(gen.data) 
 
-mbal<-read.csv("WGMS-FoG-2015-11-EE-MASS-BALANCE.csv", stringsAsFactors = F) # want the Mass Balance
-latlong <-read.csv("WGMS-FoG-2015-11-A-GENERAL-INFORMATION.csv")
-id.data <- merge(mbal, latlong, by = "WGMS_ID") 
-###years 
-
-#Alaska <-id.data %>% filter(., GEN_LOCATION == "ALASKA RANGE") %>% select(.,NAME.x, YEAR, 
-                                         # ANNUAL_BALANCE, LATITUDE, LONGITUDE)
-
-#GULKANA <- Al.Gulkana %>% filter(., NAME.x == "GULKANA") %>% mutate(., CUMULATIVE_BAL = cumsum(ANNUAL_BALANCE))
-
-NE.Gr <- id.data %>% filter(., YEAR == 2008:2014) %>% select(., NAME.x, YEAR, ANNUAL_BALANCE, 
-GEN_LOCATION, SPEC_LOCATION, LATITUDE, LONGITUDE,GEO.REGION_CODE)
-
-#isolate FREYA glacier for analysis 
-FREYA <- NE.Gr %>% filter(.,GEN_LOCATION == "NE GREENLAND") %>% 
-  mutate(., CUMULATIVE_BAL = cumsum(ANNUAL_BALANCE))
-
-#year issue
-
-Line <- gvisLineChart(FREYA, xvar ="YEAR", yvar=c("ANNUAL_BALANCE", "CUMULATIVE_BAL"),
-                      options=list(title="Freya Glacier",
-                                   titleTextStyle="{color:'blue',fontName:'Courier',fontSize:16}"))  
-###### need to edit line chart
-#checkox
-
-#Line1 <- gvisLineChart(FREYA, xvar ="YEAR",
- #                     yvar="CUMULATIVE_BAL")
-
-glacP <- readShapePoly(fn = "glims_download_28564/glims_polygons.shp") ##unable to use dplyr on spatial data frames
-glacP <- gBuffer(glacP, byid=TRUE, width=0)
-glac.NE  <- glacP[glacP$geog_area == "Northeast Greenland",]
-
-glac.choice <- as.data.frame("Northeast Greenland", "Alaska")
-maap <- ggplot2::fortify(glac.NE, region = "glac_name")
-
-maap$id = "NE GREENLAND"
-MapDf <- merge(maap, NE.Gr, by.x="id", by.y="GEN_LOCATION") 
-
-MapDf %>% ggvis(~long, ~lat) %>% group_by(group, id) %>% layer_paths(strokeOpacity:=0.5, 
-stroke:="#7f7f7f") %>% hide_legend("fill") %>% hide_axis("x") %>% hide_axis("y")%>%  set_options(width=400, height=600, keep_aspect=TRUE)
-
-############## refer to server.R
-MapDf$colour <- ifelse(MapDf$id == 'NE GREENLAND', 'blue') 
-# need to scale it by year tho, sliderinput ?
-
-Glacier<-MapDf %>% group_by(group, id)%>%
-  ggvis(~long, ~lat) %>%
-  layer_paths(fill:= ~colour)
-################
--- Project2-Shiny/Glaciers_Wanda/Glaciers_Wanda/ui.R
+++ Project2-Shiny/Glaciers_Wanda/Glaciers_Wanda/ui.R
@@ -1,21 +1,42 @@
 ## ui.R ##
-library(shiny)
-library(shinydashboard)
-library(googleVis)
-library(ggvis)
 
-shinyUI(dashboardPage(
-  dashboardHeader(title = "Glacier statistics"),
+###*** it does show the outline, need to clarify shading slider
+#glac.choice <- unique(as.character(glacP$geog_area))
+glacier.choice<-c("East Central Greenland","Northeast Greenland","Iceland", "Northern Cordillera", "Alaska")
+#Map: keep only the ones that work
+
+locations <- unique(as.character(NE.Gr$GEN_LOCATION))
+
+shinyUI(dashboardPage( skin = "purple",
+  dashboardHeader(title = "Glacier Mass Balance", titleWidth=230),
   dashboardSidebar(
     sidebarUserPanel("Wanda Wang",
-                     image = "https://avatars1.githubusercontent.com/u/10689720?v=3&s=400"),
+                     image = "http://2igww43ul7xe3nor5h2ah1yq.wpengine.netdna-cdn.com/wp-content/uploads/2016/05/IMG_2035-2-300x300.jpg"),
     sidebarMenu(
-      menuItem("Map", tabName = "map", icon = icon("map"))
-     )
+      menuItem("WorldMap", tabName = "world", icon = icon("globe")),
+      menuItem("Map", tabName = "map", icon = icon("map")),
+      menuItem("Time Series", tabName = "timeseries", icon = icon("line-chart"))
+     # menuItem("Data Source", tabName = "timeseries", icon = icon("database")),
+     ) 
+  
+    # need a color legend for mass balance/representation of melting behavior/animation
    ),
+  
   dashboardBody(
-    tabItems(
-      tabItem(tabName = "map", 
+  tabItems(
+    tabItem(tabName = "world", 
+          fluidRow(
+            box(" The World Glacier Monitoring Service (WGMS) reports data on 3681 glaciers around the world. 
+             The available location data is plotted on a global scale below."
+            ),
+              box(
+                title = "Glacier map", status = "success", 
+                solidHeader = TRUE, collapsible = TRUE,
+                leafletOutput("plotworld"), 
+                width = 12)
+            )
+              ),
+    tabItem(tabName = "map", 
         fluidRow(
           box(
             title = "Select Glacier", status = "info", 
@@ -23,47 +44,57 @@ shinyUI(dashboardPage(
             
             selectizeInput("selected",
                          "Select Glacier to Display",
-                         glac.choice)), ################add more later!!!!!!
+                         choices = glacier.choice,
+                         selected = "Northeast Greenland"
+                         )
+            ),      
           
-          box(
-            title = "Glacier map", status = "success", 
-            solidHeader = TRUE, collapsible = TRUE,
-            
-           # ifelse(sliderInput == 2000:2009, uiOutpul("plot1_ui"), )
-            uiOutput("plot1_ui"),
-            ggvisOutput("plot1") 
-            # how to iniate/connect this with sliderInput???
-            
-            #####
+          box("Crucial to the survival of a glacier is its mass balance or surface mass balance (SMB), 
+             the difference between accumulation and ablation (sublimation and melting). Climate change may cause variations in both 
+             temperature and snowfall, causing changes in the surface mass balance"
           )
         ),
-        
         fluidRow(
-          box(
-            "Select Year",
-            sliderInput("slider", "Slider input:",
-                        2000, 2014, value = c(2000, 2014), sep = "")
-          )
-          #server.R
-        ),
-        
-        fluidRow(
-        mainPanel(
-          tableOutput("values") #displays a table with the selected slider years
-          #instead need to output the value and feed it into the glacier graph
+                 box(
+                   title = "Glacier Viewer", status = "success", 
+                   solidHeader = TRUE, collapsible = TRUE,
+                   leafletOutput("plot1"), 
+                 width = 12),
+                 box(
+                 sliderInput("slider", "Annual Melt", 
+                             min = 2008, max = 2014, value = 0, step = 1, sep = "",
+                             format="$#,##0", locale="us", animate=TRUE)
+                 )
+                 
         )
-        ),
-        
-        fluidRow(
-          box(
-            title = "Time Series Chart", status = "primary",
-            solidHeader = TRUE,collapsible = TRUE,
-            htmlOutput("LineChart"),
-            width = 15) 
-        )
-        ###add in leaflet?
-        ###add in bubble chart? 
-        ##plotly
+       ),
+       
+    tabItem(tabName = "timeseries", 
+            fluidRow(
+              box(
+                title = "Select Glacier", status = "info", 
+                solidHeader = TRUE, collapsible = TRUE,
+                
+                selectizeInput("selectedts",
+                               "Select Glacier Location",
+                               locations,
+                               selected = "NE GREENLAND"
+              )),
+              
+              box("Crucial to the survival of a glacier is its mass balance or surface mass balance (SMB), 
+             the difference between accumulation and ablation (sublimation and melting). Climate change may cause variations in both 
+                  temperature and snowfall, causing changes in the surface mass balance"
+              )
+            ),
+            #image 
+           fluidRow(
+             box(
+               title = "Time Series Chart", status = "primary",
+               solidHeader = TRUE, collapsible = TRUE,
+               htmlOutput("LineChart"),   #probelem with dates, years of data being different between glaciers.
+               width = 15
+               ) 
+           )
     )
   ))
 ))

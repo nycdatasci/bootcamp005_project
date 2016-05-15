a0bd65e32c33d849a5ee7d6ca6a5925032b540d5
denistnguyen <denistnguyen@gmail.com>
2016-05-15 16:50:50 -0400

    PopulationProjection

--- Project2-Shiny/DenisNguyen/global.R
+++ Project2-Shiny/DenisNguyen/global.R
@@ -1,14 +1,22 @@
-# Load all libraries used
-
+########## Load all required libraries ##########
+library(plyr)
 library(dplyr)
 library(shiny)
 library(googleVis)
 library(datasets)
 library(shinythemes)
 library(dygraphs)
+library(Hmisc)
 
+
+########## Load data sets ##########
 dat2014 <- readRDS("data/dat2014.rds")
+dat2012 <- readRDS("data/dat2012.rds")
+dat2008 <- readRDS("data/dat2008.rds")
+spop <- readRDS("data/spop.rds")
 
+
+########## Define variables to be used in lists for choices ##########
 race <- c(
   "All" = 0,
   "White" = 1, 
@@ -16,11 +24,13 @@ race <- c(
   "Asian" = 4,
   "American Indian/Alaska Native" = 3,
   "Native Hawaiian/Other Pacific Islander" = 5,
-  "Two or more" = 6
+  "Two or more" = 6,
+  "Not Hispanic" = 7,
+  "Hispanic" = 8
 )
 
 sex <- c(
-  "All" = 0,
+  "Both" = 0,
   "Male" = 1,
   "Female" = 2
 )
@@ -29,5 +39,10 @@ population <- c(
   "Total" = "total population",
   "Births" = "births",
   "Deaths" = "deaths",
-  "Migrants" = "migrants"
+  "Net Migration" = "migration"
+)
+
+change <- c(
+  "Percent Difference" = "percent",
+  "Total Difference" = "total"
 )
--- Project2-Shiny/DenisNguyen/server.R
+++ Project2-Shiny/DenisNguyen/server.R
@@ -1,6 +1,8 @@
 shinyServer(function(input, output) {
-  
+
+### Code for Insight: Population tab
   graphInput <- reactive({
+# Choosing information based on widget inputs
     graphdat <- dat2014
     graphdat <- graphdat[graphdat$race == input$race,]
     graphdat <- graphdat[graphdat$sex == input$sex,]
@@ -16,36 +18,38 @@ shinyServer(function(input, output) {
     if ("deaths" %in% input$population) {
       i[length(i)+1] <- 5
     }
-    if ("migrants" %in% input$population) {
+    if ("migration" %in% input$population) {
       i[length(i)+1] <- 92
     }
     graphdat[, c(3,i)]
   })
-  
+# Graphing data of interest
   output$plot <- renderGvis({
     gvisLineChart(
       graphInput(), options=list(
         lineWidth=3, pointSize=5,
         vAxis="{title:'Population'}",
         hAxis="{title:'Year'}", 
-        width=950, height=800)) 
+        width=950, height=750)) 
   })
 
+
+### Code for Insight: Gender tab
   graphInputG <- reactive({
+# Choosing information based on widget inputs
     graphdatG <- dat2014
     graphdatG <- graphdatG[graphdatG$race == input$raceG,]
     h <- switch(input$populationG,
                 "total population" = 179,
                 "births" = 4,
                 "deaths" = 5,
-                "migrants" = 92)
+                "migration" = 92)
     graphdatG <- graphdatG[,c(2,3,h)]
     graphdatGG <- data.frame(graphdatG[1:47,2])
     colnames(graphdatGG)[1] <- "year"
-
     if ("0" %in% input$sexG) {
       graphdatGG <- cbind(graphdatGG,filter(graphdatG, sex == 0)[3])
-      colnames(graphdatGG)[dim(graphdatGG)[2]] <- "All"
+      colnames(graphdatGG)[dim(graphdatGG)[2]] <- "Both"
     }
     if ("1" %in% input$sexG) {
       graphdatGG <- cbind(graphdatGG,filter(graphdatG, sex == 1)[3])
@@ -59,28 +63,30 @@ shinyServer(function(input, output) {
                                graphdatGG$year <= input$yearG[2],]
     data.frame(graphdatGG)
   })
-  
+# Graphing data of interest
   output$plotG <- renderGvis({
     gvisLineChart(
       graphInputG(), options=list(
         lineWidth=3, pointSize=5,
         vAxis="{title:'Population'}",
         hAxis="{title:'Year'}", 
-        width=950, height=800))
+        width=950, height=750))
   })
-########  
+
+  
+### Code for Insight: Race tab
   graphInputR <- reactive({
+# Choosing information based on widget inputs
     graphdatR <- dat2014
     graphdatR <- graphdatR[graphdatR$sex == input$sexR,]
     t <- switch(input$populationR,
                 "total population" = 179,
                 "births" = 4,
                 "deaths" = 5,
-                "migrants" = 92)
+                "migration" = 92)
     graphdatR <- graphdatR[,c(1,3,t)]
     graphdatRR <- data.frame(graphdatR[1:47,2])
     colnames(graphdatRR)[1] <- "year"
-    
     if ("0" %in% input$raceR) {
       graphdatRR <- cbind(graphdatRR,filter(graphdatR, race == 0)[3])
       colnames(graphdatRR)[dim(graphdatRR)[2]] <- "All"
@@ -109,20 +115,114 @@ shinyServer(function(input, output) {
       graphdatRR <- cbind(graphdatRR,filter(graphdatR, race == 6)[3])
       colnames(graphdatRR)[dim(graphdatRR)[2]] <- "Two or more"
     }
+    if ("7" %in% input$raceR) {
+      graphdatRR <- cbind(graphdatRR,filter(graphdatR, race == 7)[3])
+      colnames(graphdatRR)[dim(graphdatRR)[2]] <- "Not Hispanic"
+    }
+    if ("8" %in% input$raceR) {
+      graphdatRR <- cbind(graphdatRR,filter(graphdatR, race == 8)[3])
+      colnames(graphdatRR)[dim(graphdatRR)[2]] <- "Hispanic"
+    }
     graphdatRR <- graphdatRR[graphdatRR$year >= input$yearR[1] & 
                              graphdatRR$year <= input$yearR[2],]
     data.frame(graphdatRR)
   })
-
+# Graphing data of interest
   output$plotR <- renderGvis({
     gvisLineChart(
       graphInputR(), options=list(
         lineWidth=3, pointSize=5,
         vAxis="{title:'Population'}",
         hAxis="{title:'Year'}", 
-        width=950, height=800))
+        width=950, height=750))
+  })
+
+  
+### Code for Map tab
+# Choosing information based on widget inputs
+  choice <- reactive({
+    capitalize(input$changeS)
+  })
+  lowyear <- reactive({
+    input$rangeS[1]
+  })
+  highyear <- reactive({
+    input$rangeS[2]
+  })
+# Displaying data of interest
+  output$myrange <- renderText({
+    paste(choice(), "difference from", lowyear(), "to", highyear())
+  })
+  output$plotS <- renderGvis({
+    spops <- spop[,c(grep("states", colnames(spop)),
+                     grep(input$rangeS[1], colnames(spop)),
+                     grep(input$rangeS[2], colnames(spop)))]
+    if (input$changeS == "percent") {
+      spops[,4] <- 100*(spops[,3]-spops[,2])/spops[,2]
+      colnames(spops)[4] <- "change"
+    }
+    if (input$changeS == "total") {
+      spops[,4] <- spops[,3]-spops[,2]
+      colnames(spops)[4] <- "change"
+    }
+    gvisGeoChart(spops, locationvar="states", colorvar="change",
+                 options=list(region="US", displayMode="regions", 
+                              resolution="provinces",
+                              width=900, height=650,
+                              colorAxis="{colors:['#deebf7', '#3182bd']}"))
   })
   
+  
+### Code for Projections tab
+  graphInputP <- reactive({
+# Choosing information based on widget inputs
+    graphdat2008 <- dat2008[(dat2008$race == 0) & (dat2008$sex == 0),]
+    colnames(graphdat2008)[7] <- '2008'
+    graphdat2012 <- dat2012[(dat2012$race == 0) & (dat2012$sex == 0),]
+    colnames(graphdat2012)[179] <- '2012'
+    graphdat2014 <- dat2014[(dat2014$race == 0) & (dat2014$sex == 0),]
+    colnames(graphdat2014)[179] <- '2014'
+    for (i in 1:13) {
+      graphdat2014 <- rbind(graphdat2014, c(rep(NA, dim(graphdat2014)[1])))
+      graphdat2014$year[47+i] <- 2000 + i
+    }
+    for (i in 1:11) {
+      graphdat2012 <- rbind(graphdat2012, c(rep(NA, dim(graphdat2012)[1])))
+      graphdat2012$year[49+i] <- 2000 + i
+    }
+    for (i in 1:10) {
+      graphdat2008 <- rbind(graphdat2008, c(rep(NA, dim(graphdat2008)[1])))
+      graphdat2008$year[50+i] <- 2050 + i
+    }
+    graphdatP <- merge(merge(graphdat2014[,c(3,179)], graphdat2012[,c(3,179)], by="year"), 
+                       graphdat2008[,c(3,7)], by="year")
+    q <- c()
+    if ("2008" %in% input$projectionP) {
+      q[length(q)+1] <- 4
+    }
+    if ("2012" %in% input$projectionP) {
+      q[length(q)+1] <- 3
+    }
+    if ("2014" %in% input$projectionP) {
+      q[length(q)+1] <- 2
+    }
+    graphdatP <- graphdatP[,c(1,q)]
+    graphdatP <- graphdatP[graphdatP$year >= input$yearP[1] & 
+                               graphdatP$year <= input$yearP[2],]
+    graphdatP
+  })
+# Graphing data of interest
+  output$plotP <- renderGvis({
+    gvisLineChart(
+      graphInputP(), options=list(
+        lineWidth=3, pointSize=5,
+        vAxis="{title:'Population'}",
+        hAxis="{title:'Year', format:'####'}",
+        width=950, height=750))
+  })
+
+
+### Code to display Data tab
   output$data2014 <- DT::renderDataTable(
     DT::datatable({
       data <- dat2014

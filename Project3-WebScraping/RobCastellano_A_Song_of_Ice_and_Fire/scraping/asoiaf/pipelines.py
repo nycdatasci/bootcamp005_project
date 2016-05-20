bc81b7c01a8f7dd44b0aa31001837b7d4858d417
rtcastellano <RTCastellano@gmail.com>
2016-05-20 18:06:56 -0400

    Adding Rob's scraping code

---
+++ Project3-WebScraping/RobCastellano_A_Song_of_Ice_and_Fire/scraping/asoiaf/pipelines.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+from scrapy.exceptions import DropItem
+from scrapy.conf import settings
+from scrapy import log
+import pymongo
+
+class ValidateItemPipeline(object):
+	def process_item(self, item, spider):
+		if not all(item.values()):
+			raise DropItem("Missing values!")
+		else:
+			return item
+
+
+class MongoDBPipeline(object):
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
+    def process_item(self, item, spider):
+        self.collection.insert(dict(item))
+        log.msg("Added to MongoDB database!",
+                level=log.DEBUG, spider=spider)
+        return item
